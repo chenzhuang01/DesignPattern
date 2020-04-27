26d74b8163c1fe36cc918620d361b997068d3cf3
chenzhuang01 <54887993+chenzhuang01@users.noreply.github.com>
2020-04-27 10:45:12 +0800

    Update XMLUtil.java

--- src/pattern10_VisitorPattern/XMLUtil.java
+++ src/pattern10_VisitorPattern/XMLUtil.java
@@ -1,11 +1,45 @@
 package pattern10_VisitorPattern;
 
+import java.io.File;
 import java.lang.reflect.Field;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class XMLUtil {
-	public static Object getBean() throws ClassNotFoundException, NoSuchFieldException, SecurityException {
-		Class<?> c = Class.forName("config");
-		Field className = c.getField("className");
-		return className;
-	}
+	//该方法用于从XML配置文件中提取具体类类名，并返回一个实例对象
+		public static Object getBean()
+		{
+			try
+			{
+				//创建文档对象
+				DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = dFactory.newDocumentBuilder();
+				Document doc;							
+				doc = builder.parse(new File("F://java-workspace//FactoryDemo//src//pattern10_VisitorPattern//config.xml")); 
+				NodeList nl=null;
+				Node classNode=null;
+				String cName=null;
+				nl = doc.getElementsByTagName("className");
+				
+				
+				//获取包含类名的文本节点
+		        classNode=nl.item(0).getFirstChild();
+		         
+		        cName=classNode.getNodeValue();
+		        //通过类名生成实例对象并将其返回
+		        Class c=Class.forName(cName);
+			  	Object obj=c.newInstance();
+		        return obj;		
+	           }   
+	           	catch(Exception e)
+	           	{
+	           		e.printStackTrace();
+	           		return null;
+	           	}
+			}
 }
